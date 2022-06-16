97670265ab2af2c0cf107e85bd14fefa77ee91f4
devim <developerimalka@gmail.com>
2022-06-16 21:03:09 +0530

    added cs js and images

---
+++ main.js
@@ -0,0 +1,11 @@
+imgs = document.getElementsByTagName('img')
+imgs.adde
+
+function sendValue(list){
+
+    let innerName = document.getElementById('name');
+    let innerId = document.getElementById("iconid");
+
+    innerId.value = list[0];
+    innerName.value = list[1];
+}
